fe186266d1efd69283abb1bb972fef2ec37cbd8f
Bruno D. Miranda <bdouram@gmail.com>
2022-06-19 12:38:38 -0300

    :hammer: Instructions on main file

--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@ import os
 import globals
 from time import sleep
 from random import seed
+from datetime import datetime
 
 from stars.planet import Planet
 from space.bases import SpaceBase
@@ -17,10 +18,13 @@ from mines.uranium import StoreHouse
 
 def main():
 
-    seed(10)
+    # Alimentando a semente aleatória do sistema.
+    seed(datetime.now())
     
+    # Limpando o terminal.
     os.system('cls' if os.name == 'nt' else 'clear')
 
+    # Carregando as threads dos satélites.
     sleep(0.5)
     print("Loading planets ...\n")
     mars = Planet(100, 'MARS')
@@ -28,12 +32,13 @@ def main():
     ganimedes = Planet(100, 'GANIMEDES')
     europa = Planet(100, 'EUROPA')
 
+    # Iniciando as threads dos satélites.
     mars.start()
     io.start()
     ganimedes.start()
     europa.start()
 
-
+    # Carregando as threads das bases espaciais.
     sleep(0.5)
     print("\nLoading space bases ...\n")
     alcantara = SpaceBase('ALCANTARA', 20000, 100, 1)
@@ -41,30 +46,30 @@ def main():
     moscow = SpaceBase('MOSCOW', 40000, 500, 5)
     moon = SpaceBase('MOON', 30000, 50, 2)
 
+    # Iniciando as threads das bases espaciais.
     alcantara.start()
     canaveral_cape.start()
     moscow.start()
     moon.start()
 
 
+    # Carregando as minas de combustível e urânio.
     sleep(0.5)
     print("\nActivating Mines...\n")
 
     oil_earth = Pipeline(0, 'EARTH', 500)
-    oil_moon = Pipeline(0, 'MOON', 200)
     uranium_earth = StoreHouse(0, 'EARTH', 200)
-    uranium_moon = StoreHouse(0, 'MOON', 400)
 
+    # Iniciando as minas de combustível e urânio.
     uranium_earth.start()
-    uranium_moon.start()
     oil_earth.start()
-    oil_moon.start()
 
 
-    
+    # Carregando o tempo de simulação do sistema.
     time_simulation = SimulationTime()
     time_simulation.start()
 
+    # Carregando as variáveis globais do sistema.
     bases = {
         'alcantara': alcantara,
         'canaveral_cape': canaveral_cape,
@@ -81,9 +86,7 @@ def main():
     
     mines = {
         'oil_earth': oil_earth,
-        'oil_moon': oil_moon,
         'uranium_earth': uranium_earth,
-        'uranium_moon': uranium_moon,
     }
 
     globals.set_planets_ref(planets)
@@ -91,6 +94,7 @@ def main():
     globals.set_mines_ref(mines)
     globals.set_simulation_time(time_simulation)
 
+    # Liberando a execução do sistema.
     sleep(0.5)
     print("\n\n##################################### SIMULATION STARTED #####################################\n")
     globals.set_release_system()
